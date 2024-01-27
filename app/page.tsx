a61d202071971f026497c64c91c973c158ef0ad4
Nati Peretz <nati.peretz1985@gmail.com>
2024-01-27 14:05:55 +0200

    bug fix beofre delpoy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-// export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
+// export const revalidate = 0;
 import getProducts, { IProductParams } from "../actions/getProducts";
 import Container from "../components/Container";
 import HomeBanner from "../components/HomeBanner";
